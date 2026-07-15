d8dbcfef9107f51a43b33c59aa47e5958ae35237
Meet Soni <meet.tecmantras@gmail.com>
2026-07-15 12:43:44 +0530

    Initial commit with Vue home + auth views

---
+++ frontend/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import RegisterView from '../views/RegisterView.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/login', name: 'Login', component: LoginView },
+  { path: '/register', name: 'Register', component: RegisterView },
+  { path: '/:pathMatch(.*)*', redirect: '/' }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 };
+  }
+});
+
+export default router;
